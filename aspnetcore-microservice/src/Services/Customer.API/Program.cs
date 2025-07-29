c3e6ee0586ad13040344e023e9cf91df734d2c5e
DatNguyen <dat708710@gmail.com>
2025-07-29 17:57:30 +0700

    Initial Project Customer.API

--- aspnetcore-microservice/src/Services/Customer.API/Program.cs
+++ aspnetcore-microservice/src/Services/Customer.API/Program.cs
@@ -1,4 +1,12 @@
 using Common.Logging;
+using Contracts.Common.Interfaces;
+using Customer.API.Persistence;
+using Customer.API.Repositories;
+using Customer.API.Repositories.Interfaces;
+using Customer.API.Services;
+using Customer.API.Services.Interfaces;
+using Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +21,15 @@ try
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<CustomerContext>(
+        options => options.UseNpgsql(connectionString));
+
+    builder.Services.AddScoped<ICustomerRepository, CustomerRepository>()
+        .AddScoped(typeof(IRepositoryBaseAsync<,,>), typeof(RepositoryBaseAsync<,,>))
+        .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>))
+        .AddScoped<ICustomerService, CustomerService>();
 
     var app = builder.Build();
 
@@ -28,11 +45,16 @@ try
     app.UseAuthorization();
 
     app.MapControllers();
+    
+    app.SeedCustomerData().Run();
 
     app.Run();
 }
 catch (Exception ex)
 {
+    string type = ex.GetType().Name;
+    if(type.Equals("StopTheHostException", StringComparison.Ordinal)) throw;
+    
     Log.Fatal(ex, "Unhandled exception");
 }
 finally
