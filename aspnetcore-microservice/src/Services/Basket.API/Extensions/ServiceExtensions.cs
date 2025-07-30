2bddaa7d44f0697f365b7eefc80015fcbe98d656
DatNguyen <dat708710@gmail.com>
2025-07-30 17:58:31 +0700

    Create CURD for Basket API

--- aspnetcore-microservice/src/Services/Basket.API/Extensions/ServiceExtensions.cs
+++ aspnetcore-microservice/src/Services/Basket.API/Extensions/ServiceExtensions.cs
@@ -10,4 +10,18 @@ public static class ServiceExtensions
     public static IServiceCollection ConfigureService(this IServiceCollection services) =>
         services.AddScoped<IBasketRepository, BasketRepository>()
         .AddTransient<ISerializeService, SerializeService>();
+
+    public static void ConfigureRedis(this IServiceCollection services, IConfiguration configuration)
+    {
+        var redisConnectionString = configuration.GetSection("CacheSettings: ConnectionString").Value;
+        if (string.IsNullOrEmpty(redisConnectionString))
+            throw new ArgumentException("Redis connection string is not configured.");
+
+        //Redis cache configuration
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = redisConnectionString;
+            options.InstanceName = "BasketAPI";
+        });
+    }
 }
